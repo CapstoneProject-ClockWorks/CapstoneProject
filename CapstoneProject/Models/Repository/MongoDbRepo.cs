9676751a781cc1b1f70f3edcd5f151817454348f
PhuongDuy <bixoan2101@gmail.com>
2018-11-01 15:19:53 +0700

    Start project, connect mlab database

---
+++ CapstoneProject/Models/Repository/MongoDbRepo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MongoDB.Driver;
+
+namespace CapstoneProject.Models.Repository
+{
+    public class MongoDbRepo
+    {
+        //The client that manage the connection
+        public MongoClient Client;
+        //The interface that manage the database
+        public IMongoDatabase Db;
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="url">Mongo server url</param>
+        /// <param name="database">Database name</param>
+        public MongoDbRepo(string url, string database)
+        {
+            this.Client = new MongoClient(url);
+            //if the database is not exist, creates the database
+            this.Db = this.Client.GetDatabase(database);
+        }
+    }
+}
---
+++ CapstoneProject/Models/WorkSpace.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace CapstoneProject.Models
+{
+    public class WorkSpace
+    {
+        [BsonId]
+        public ObjectId _id { get; set; }
+        [BsonElement("workspace_name")]
+        public string workspace_name { get; set; }
+        [BsonElement("decprition")]
+        public string decprition { get; set; }
+        [BsonElement("image")]
+        public string image { get; set; }
+    }
+}
