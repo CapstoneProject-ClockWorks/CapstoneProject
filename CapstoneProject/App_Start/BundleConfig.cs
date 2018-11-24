28ef989b084ea03fffc79dc10e8abb4ad81df867
Nguyễn Đình Quốc An <annguyendinhquoc115@gmail.com>
2018-11-24 16:08:16 +0700

    fix menu,add login language

--- CapstoneProject/App_Start/BundleConfig.cs
+++ CapstoneProject/App_Start/BundleConfig.cs
@@ -8,7 +8,7 @@ namespace CapstoneProject
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                 "~/Content/libs/font-awesome-4/css/font-awesome.min.css",
                 "~/Content/libs/font-awesome-5/css/all.css",
@@ -19,10 +19,7 @@ namespace CapstoneProject
             bundles.Add(new ScriptBundle("~/Content/js").Include(
                 "~/Content/libs/jquery/dist/jquery.min.js",
                 "~/Content/libs/bootstrap/dist/js/bootstrap.min.js",
-                "~/Content/libs/font-awesome-5/js/all.js",
-                "~/Content/build/js/common.js",
-                "~/Content/build/js/custom.min.js",
-                "~/Content/build/js/fixHeight.js"
+                "~/Content/build/js/custom.min.js"
             ));
 
         }
