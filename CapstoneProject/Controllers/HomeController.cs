725bb54c050754d4a7f91114447b2a3678e59522
PhuongDuy <bixoan2101@gmail.com>
2018-11-26 22:02:07 +0700

    Update project

--- CapstoneProject/Controllers/HomeController.cs
+++ CapstoneProject/Controllers/HomeController.cs
@@ -11,11 +11,13 @@ using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace CapstoneProject.Controllers
 {
-    public class HomeController : BaseController
+    public class HomeController : Controller
     {
         CapstoneProjectModelEntities db = new CapstoneProjectModelEntities();
         public ActionResult Index()
         {
+            string user = User.Identity.GetUserName();
+            Session["UserName"] = user;
             var viewlistworkspace = db.WorkSpaces.OrderByDescending(m => m.ID).ToList();
             return View(viewlistworkspace);
         }
@@ -51,16 +53,16 @@ namespace CapstoneProject.Controllers
         [HttpPost]
         public ActionResult CreateWorkSpace(WorkSpace ws)
         {
-			if (!ws.Equals(null))
-			{
-				ws.Createdate = DateTime.Now;
-				//ws.Bilimail = Session["UserName"].ToString();
-				db.WorkSpaces.Add(ws);
-				db.SaveChanges();
-				return RedirectToAction("Index");
-			}
-			else
-				return View();
+            ws.Createdate = DateTime.Now;
+            WS_User_Roles wsp = new WS_User_Roles();
+            string userid = User.Identity.GetUserId();
+            Session["UserId"] = userid;
+            wsp.User_ID = userid;
+            wsp.Role_ID = 4;
+            db.WS_User_Roles.Add(wsp);
+            db.WorkSpaces.Add(ws);
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
 
         public ActionResult DeleteWorkSpace(WorkSpace ws)
@@ -88,7 +90,7 @@ namespace CapstoneProject.Controllers
         }
 
         [HttpPost]
-        public ActionResult AddMemberWS(WorkSpace model,List<string> adduser)
+        public ActionResult AddMemberWS(WorkSpace model, List<string> adduser)
         {
             WorkSpace wp = db.WorkSpaces.Find(model.ID);
             foreach (var user in adduser)
@@ -116,14 +118,36 @@ namespace CapstoneProject.Controllers
             ws.WorkSpaceName = model.WorkSpaceName;
             ws.Description = model.Description;
             db.SaveChanges();
-            return RedirectToAction("ShowWorkSpace");
+            return RedirectToAction("ShowWorkSpace", new { id = model.ID });
         }
 
-        public ActionResult EditRoleMemberWS()
+        public ActionResult EditRoleMemberWS(int? id)
         {
-            return View();
+            WS_User_Roles ws = db.WS_User_Roles.Find(id);
+            ViewBag.editrole = db.RoleWorkSpaces.OrderByDescending(x => x.ID).ToList();
+            return View(ws);
         }
 
+        [HttpPost]
+        public ActionResult EditRoleMemberWS(WS_User_Roles model, List<string> chk1)
+        {
+            WS_User_Roles wsp = db.WS_User_Roles.Find(model.ID);
+            //var roleuser = db.WS_User_Roles.Where(x => x.WorkSpace_ID == model.ID).ToList();
+            //db.WS_User_Roles.RemoveRange(roleuser);
+            //foreach (var fe in chk1)
+            //{
+            //    WS_User_Roles profe = new WS_User_Roles();
+            //    profe.Role_ID = db.RoleWorkSpaces.SingleOrDefault(x => x.RoleName == fe).ID;
+            //    profe.WorkSpace_ID = wsp.WorkSpace_ID;
+            //    db.WS_User_Roles.Add(profe);
+            //}
+
+            wsp.Role_ID = model.Role_ID;
+
+            db.Entry(wsp).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("AddMemberWS", new { id = wsp.WorkSpace_ID });
+        }
 
     }
 }
