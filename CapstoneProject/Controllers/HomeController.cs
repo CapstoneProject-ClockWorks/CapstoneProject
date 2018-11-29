bc21144c0c1571c2c1c35ce4d898253633edd36d
Nguyễn Đình Quốc An <annguyendinhquoc115@gmail.com>
2018-11-29 21:37:17 +0700

    fix login

--- CapstoneProject/Controllers/HomeController.cs
+++ CapstoneProject/Controllers/HomeController.cs
@@ -32,149 +32,145 @@ namespace CapstoneProject.Controllers
             return View();
         }
 
-<<<<<<< HEAD
-        public ActionResult CreateWorkSpace()
-        {
-            if (Request.IsAuthenticated)
-            {
-                string user = User.Identity.GetUserName();
-                Session["UserName"] = user;
-                return View();
-            }
-            else
-            {
-                return RedirectToAction("Login", "Account");
-            }
-        }
+        //public ActionResult CreateWorkSpace()
+        //{
+        //    if (Request.IsAuthenticated)
+        //    {
+        //        string user = User.Identity.GetUserName();
+        //        Session["UserName"] = user;
+        //        return View();
+        //    }
+        //    else
+        //    {
+        //        return RedirectToAction("Login", "Account");
+        //    }
+        //}
 
-        [HttpPost]
-        public ActionResult CreateWorkSpace(WorkSpace ws, HttpPostedFileBase ImageWS)
-        {
-            if (ImageWS != null)
-            {
-                string avatar = "";
-                if (ImageWS.ContentLength > 0)
-                {
-                    var filename = Path.GetFileName(ImageWS.FileName);
-                    var path = Path.Combine(Server.MapPath("~/Images/"), filename);
-                    ImageWS.SaveAs(path);
-                    avatar = filename;
-                }
-                ws.ImageWS = avatar;
-            }
+        //[HttpPost]
+        //public ActionResult CreateWorkSpace(WorkSpace ws, HttpPostedFileBase ImageWS)
+        //{
+        //    if (ImageWS != null)
+        //    {
+        //        string avatar = "";
+        //        if (ImageWS.ContentLength > 0)
+        //        {
+        //            var filename = Path.GetFileName(ImageWS.FileName);
+        //            var path = Path.Combine(Server.MapPath("~/Images/"), filename);
+        //            ImageWS.SaveAs(path);
+        //            avatar = filename;
+        //        }
+        //        ws.ImageWS = avatar;
+        //    }
 
-            ws.Createdate = DateTime.Now;
-            WS_User_Roles wsp = new WS_User_Roles();
-            string userid = User.Identity.GetUserId();
-            Session["UserId"] = userid;
-            wsp.User_ID = userid;
-            wsp.Role_Admin = true;
-            wsp.Role_Member = true;
-            db.WS_User_Roles.Add(wsp);
-            db.WorkSpaces.Add(ws);
-            db.SaveChanges();
-            return RedirectToAction("Index");
+        //    ws.Createdate = DateTime.Now;
+        //    WS_User_Roles wsp = new WS_User_Roles();
+        //    string userid = User.Identity.GetUserId();
+        //    Session["UserId"] = userid;
+        //    wsp.User_ID = userid;
+        //    wsp.Role_Admin = true;
+        //    wsp.Role_Member = true;
+        //    db.WS_User_Roles.Add(wsp);
+        //    db.WorkSpaces.Add(ws);
+        //    db.SaveChanges();
+        //    return RedirectToAction("Index");
 
 
-        }
+        //}
 
-        public ActionResult DeleteWorkSpace(WorkSpace ws)
-        {
-            WorkSpace worksp = db.WorkSpaces.Find(ws.ID);
-            var iduser = db.WS_User_Roles.Where(x => x.WorkSpace_ID == ws.ID);
-            db.WS_User_Roles.RemoveRange(iduser);
-            db.WorkSpaces.Remove(worksp);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
+        //public ActionResult DeleteWorkSpace(WorkSpace ws)
+        //{
+        //    WorkSpace worksp = db.WorkSpaces.Find(ws.ID);
+        //    var iduser = db.WS_User_Roles.Where(x => x.WorkSpace_ID == ws.ID);
+        //    db.WS_User_Roles.RemoveRange(iduser);
+        //    db.WorkSpaces.Remove(worksp);
+        //    db.SaveChanges();
+        //    return RedirectToAction("Index");
+        //}
 
-        public ActionResult ShowWorkSpace(int? id)
-        {
-            WorkSpace showws = db.WorkSpaces.Find(id);
-            if (showws == null)
-            {
-                return HttpNotFound();
-            }
-            return View(showws);
-        }
+        //public ActionResult ShowWorkSpace(int? id)
+        //{
+        //    WorkSpace showws = db.WorkSpaces.Find(id);
+        //    if (showws == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(showws);
+        //}
 
-        public ActionResult AddMemberWS(int? id)
-        {
-            WorkSpace addmem = db.WorkSpaces.Find(id);
-            ViewBag.listuser = db.AspNetUsers.OrderByDescending(x => x.Id).ToList();
-            return View(addmem);
-        }
+        //public ActionResult AddMemberWS(int? id)
+        //{
+        //    WorkSpace addmem = db.WorkSpaces.Find(id);
+        //    ViewBag.listuser = db.AspNetUsers.OrderByDescending(x => x.Id).ToList();
+        //    return View(addmem);
+        //}
 
-        [HttpPost]
-        public ActionResult AddMemberWS(WorkSpace model, List<string> adduser)
-        {
-            WorkSpace wp = db.WorkSpaces.Find(model.ID);
-            //var idwp = db.WS_User_Roles.Where(x => x.WorkSpace_ID == model.ID);
-            //db.WS_User_Roles.RemoveRange(idwp);
-            foreach (var user in adduser)
-            {
-                WS_User_Roles wsuser = new WS_User_Roles();
-                var id = db.AspNetUsers.SingleOrDefault(x => x.Id == user);
-                var rs = db.WS_User_Roles.ToList();
-                foreach (var item in rs)
-                {
-                    if (item.User_ID.Equals(id) == true)
-                    {
+        //[HttpPost]
+        //public ActionResult AddMemberWS(WorkSpace model, List<string> adduser)
+        //{
+        //    WorkSpace wp = db.WorkSpaces.Find(model.ID);
+        //    //var idwp = db.WS_User_Roles.Where(x => x.WorkSpace_ID == model.ID);
+        //    //db.WS_User_Roles.RemoveRange(idwp);
+        //    foreach (var user in adduser)
+        //    {
+        //        WS_User_Roles wsuser = new WS_User_Roles();
+        //        var id = db.AspNetUsers.SingleOrDefault(x => x.Id == user);
+        //        var rs = db.WS_User_Roles.ToList();
+        //        foreach (var item in rs)
+        //        {
+        //            if (item.User_ID.Equals(id) == true)
+        //            {
 
-                    }
-                    else
-                    {
-                        wsuser.User_ID = db.AspNetUsers.SingleOrDefault(x => x.Email == user).Id;
+        //            }
+        //            else
+        //            {
+        //                wsuser.User_ID = db.AspNetUsers.SingleOrDefault(x => x.Email == user).Id;
 
-                        wsuser.WorkSpace_ID = wp.ID;
-                        wsuser.Role_Member = true;
-                        db.WS_User_Roles.Add(wsuser);
-                        db.SaveChanges();
-                    }
-                }
+        //                wsuser.WorkSpace_ID = wp.ID;
+        //                wsuser.Role_Member = true;
+        //                db.WS_User_Roles.Add(wsuser);
+        //                db.SaveChanges();
+        //            }
+        //        }
 
-            }
+        //    }
 
-            return RedirectToAction("AddMemberWS", new { id = model.ID });
-        }
+        //    return RedirectToAction("AddMemberWS", new { id = model.ID });
+        //}
 
-        public ActionResult SettingWorkSpace(int? id)
-        {
-            WorkSpace editws = db.WorkSpaces.Find(id);
-            return View(editws);
-        }
+        //public ActionResult SettingWorkSpace(int? id)
+        //{
+        //    WorkSpace editws = db.WorkSpaces.Find(id);
+        //    return View(editws);
+        //}
 
-        [HttpPost]
-        public ActionResult SettingWorkSpace(WorkSpace model)
-        {
-            WorkSpace ws = db.WorkSpaces.Find(model.ID);
-            ws.WorkSpaceName = model.WorkSpaceName;
-            ws.Description = model.Description;
-            db.SaveChanges();
-            return RedirectToAction("ShowWorkSpace", new { id = model.ID });
-        }
+        //[HttpPost]
+        //public ActionResult SettingWorkSpace(WorkSpace model)
+        //{
+        //    WorkSpace ws = db.WorkSpaces.Find(model.ID);
+        //    ws.WorkSpaceName = model.WorkSpaceName;
+        //    ws.Description = model.Description;
+        //    db.SaveChanges();
+        //    return RedirectToAction("ShowWorkSpace", new { id = model.ID });
+        //}
 
-        public ActionResult EditRoleMemberWS(int? id)
-        {
-            WS_User_Roles ws = db.WS_User_Roles.Find(id);
-            return View(ws);
-        }
+        //public ActionResult EditRoleMemberWS(int? id)
+        //{
+        //    WS_User_Roles ws = db.WS_User_Roles.Find(id);
+        //    return View(ws);
+        //}
 
-        [HttpPost]
-        public ActionResult EditRoleMemberWS(WS_User_Roles model, List<string> chk1)
-        {
-            WS_User_Roles wsp = db.WS_User_Roles.Find(model.ID);
-            wsp.Role_Admin = model.Role_Admin;
-            wsp.Role_Manager = model.Role_Manager;
-            wsp.Role_Member = model.Role_Member;
+        //[HttpPost]
+        //public ActionResult EditRoleMemberWS(WS_User_Roles model, List<string> chk1)
+        //{
+        //    WS_User_Roles wsp = db.WS_User_Roles.Find(model.ID);
+        //    wsp.Role_Admin = model.Role_Admin;
+        //    wsp.Role_Manager = model.Role_Manager;
+        //    wsp.Role_Member = model.Role_Member;
 
-            db.Entry(wsp).State = System.Data.Entity.EntityState.Modified;
-            db.SaveChanges();
-            return RedirectToAction("AddMemberWS", new { id = wsp.WorkSpace_ID });
-        }
-=======
-        
->>>>>>> 01da5a41649de82467529688bf5d5d87b897f9db
+        //    db.Entry(wsp).State = System.Data.Entity.EntityState.Modified;
+        //    db.SaveChanges();
+        //    return RedirectToAction("AddMemberWS", new { id = wsp.WorkSpace_ID });
+        //}
 
     }
 }
--- CapstoneProject/Models/AccountViewModels.cs
+++ CapstoneProject/Models/AccountViewModels.cs
@@ -65,11 +65,6 @@ namespace CapstoneProject.Models
     public class RegisterViewModel
     {
         [Required]
-        [StringLength(30, ErrorMessage = "The {0} must be at least {2} characters long.")]
-        [Display(Name = "Username")]
-        public string Username { get; set; }
-
-        [Required]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
